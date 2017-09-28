7e52b6d25e230a2fb5bd97868e16c516f3b78d5d
nguyensontung1402 <tungga1236@gmail.com>
2017-09-28 20:56:14 +0700

    bo 2 dong  import

--- computerlabs/src/java/computerlabs/dbconnect.java
+++ computerlabs/src/java/computerlabs/dbconnect.java
@@ -11,8 +11,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.sql.*;
-import javax.naming.*;
+
 
 /**
  *
